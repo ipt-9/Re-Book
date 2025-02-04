9d109509467e363c6850bcf02e500ad74a7e81bf
Laura Gjoni <Laura_Gjoni@sluz.ch>
2025-02-04 17:05:11 +0100

    Homepage framework created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,32 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { SearchComponent } from './pages/search/search.component';
+import { SellComponent } from './pages/sell/sell.component';
+import { BrowseComponent } from './pages/browse/browse.component';
+import { ProductComponent } from './pages/product/product.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { CartComponent } from './pages/cart/cart.component';
+import { CheckoutComponent } from './pages/checkout/checkout.component';
+import { TermsComponent } from './pages/terms/terms.component';
+import { PolicyComponent } from './pages/policy/policy.component';
+import { FavoritesComponent } from './pages/favorites/favorites.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'search', component: SearchComponent },
+  { path: 'sell', component: SellComponent },
+  { path: 'browse', component: BrowseComponent },
+  { path: 'product/:id', component: ProductComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'cart', component: CartComponent },
+  { path: 'checkout', component: CheckoutComponent },
+  { path: 'terms', component: TermsComponent },
+  { path: 'policy', component: PolicyComponent },
+  { path: 'favorites', component: FavoritesComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: '**', redirectTo: 'home' }
+];
