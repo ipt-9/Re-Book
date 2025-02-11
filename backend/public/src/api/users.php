6d70b874b504b281505ea686585daba6ee8eb51b
Alex Belik <alex.belik26@gmail.com>
2025-02-11 17:00:11 +0100

    database connection

---
+++ backend/public/src/api/users.php
@@ -0,0 +1,13 @@
+﻿<?php
+require '../config.php';
+
+header('Content-Type: application/json');
+
+try {
+    $stmt = $pdo->query("SELECT * FROM users");
+    $books = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    echo json_encode($books);
+} catch (PDOException $e) {
+    echo json_encode(["error" => $e->getMessage()]);
+}
+?>
