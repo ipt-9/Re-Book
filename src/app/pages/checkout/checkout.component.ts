8df72d28084df3ed2f0d3c46051920041759f9e5
Laura Gjoni <Laura_Gjoni@sluz.ch>
2025-03-11 16:56:04 +0100

    Finished Design

--- src/app/pages/checkout/checkout.component.ts
+++ src/app/pages/checkout/checkout.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
-import {RouterLink} from "@angular/router";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-checkout',
-    imports: [
-        RouterLink
-    ],
   templateUrl: './checkout.component.html',
-  styleUrls: ['./checkout.component.scss', '../../../styles.scss']
+  styleUrls: ['./checkout.component.scss']
 })
 export class CheckoutComponent {
+  constructor(private router: Router) {}
 
+  goToConfirmation(): void {
+    this.router.navigate(['/confirmation']);
+  }
 }
--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,45 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthService } from './auth.service';
-import {Router, RouterLink} from '@angular/router';
-import {NgForOf, NgIf} from '@angular/common';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-profile',
+  imports: [],
   templateUrl: './profile.component.html',
-  imports: [
-    RouterLink,
-    NgIf,
-    NgForOf
-  ],
-  styleUrls: ['./profile.component.scss']
+  styleUrl: './profile.component.scss'
 })
-export class ProfileComponent implements OnInit {
-  user: any = null;
+export class ProfileComponent {
 
-  constructor(private authService: AuthService, private router: Router) {}
-
-  ngOnInit() {
-    this.authService.getUserData().subscribe(
-      (response) => {
-        if (response.status === 'success') {
-          this.user = response.user;
-        } else {
-          this.router.navigate(['/login']); // Redirect if not logged in
-        }
-      },
-      (error) => {
-        console.error('Error fetching user data', error);
-        this.router.navigate(['/login']); // Redirect if error occurs
-      }
-    );
-  }
-
-  logout() {
-    this.authService.logout().subscribe(response => {
-      if (response.status === 'success') {
-        this.user = null;
-        this.router.navigate(['/login']); // Redirect after logout
-      }
-    });
-  }
 }
