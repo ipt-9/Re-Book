e53e0c2ed21727391e5105fa4023bd97a9abca5d
Alex Belik <alex.belik26@gmail.com>
2025-05-03 15:05:54 +0200

    cart impovement

--- src/app/pages/favorites/favorites.component.ts
+++ src/app/pages/favorites/favorites.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit  } from '@angular/core';
 import {RouterLink} from "@angular/router";
 import { NavbarComponent } from '../navbar/navbar.component';
 import { FooterComponent } from '../footer/footer.component';
+import { FavoritesService } from './favorites.service';
+import {DecimalPipe} from '@angular/common';
 
 @Component({
   selector: 'app-favorites',
@@ -9,12 +11,19 @@ import { FooterComponent } from '../footer/footer.component';
   imports: [
     RouterLink,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    DecimalPipe
   ],
 
   templateUrl: './favorites.component.html',
   styleUrls: ['./favorites.component.scss', '../../../styles.scss']
 })
-export class FavoritesComponent {
+export class FavoritesComponent implements OnInit {
+  favourites: any[] = [];
 
+  constructor(private favoritesService: FavoritesService) {}
+
+  ngOnInit(): void {
+    this.favourites = this.favoritesService.getAll();
+  }
 }
